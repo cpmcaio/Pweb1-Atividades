0fa22f1fbeedb6339039485cbf05eb1702ffa883
cpmcaio <caiocpmelo2@gmail.com>
2026-03-24 23:30:58 -0300

    finalizando atividade objeto exames

--- Objeto Exames/index.ts
+++ Objeto Exames/index.ts
@@ -1,23 +1,36 @@
-interface Weight{
-    valor: number
+class Weight{
+    constructor(public value: number[]){}
 }
 
-interface Answer{
-    valor: number
+class Answer{
+    constructor(
+        public student: string,
+        public responses: string[]
+    ){}
 }
 class Exams{
     private weight: Weight;
-    private answer: Answer;
+    private answerkey: Answer;
     private exams: Array<Answer> = [];
-    constructor(answer:Answer, weight: Weight){
-        this.answer =answer
+    
+    constructor(answerkey:Answer, weight: Weight){
+        this.answerkey =answerkey
         this.weight = weight
     }
-    
-    public add(exam:Answer): void{
-        this.exams.push(exam)
+        public add(exam: Answer){
+            this.exams.push(exam);
+        }
+        private calcularnota(exam: Answer):number{
+            let notafinal = 0;
+            for (let i = 0; i < exam.responses.length; i++){
+                if (this.exams[i] == this.answerkey.responses[i]){
+                    notafinal += this.weight.value[i]??0; 
+                }
+            }
+            return notafinal;
+        }
+
+        public avg(): number{
+            const media = this.exams.reduce((total, valor) => total + valor, 0) / this.exams.length;   
+        }
     }
-    public avg(): number{
-        return 0;
-    }
-}
